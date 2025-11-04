0b77df192cc884dbd2f5657441ff34fc32bba2e6
SATISHSP-05 <satishsp0503@gmail.com>
2025-11-05 01:02:17 +0530

    practise of promise

---
+++ promise.js
@@ -0,0 +1,41 @@
+// promise resolve reject 
+let promise1 = new Promise((resolve, reject)=>{
+    let success = false;
+    if(success){
+        resolve("seccess");
+    } else
+    {
+        reject("failed");
+    }
+});
+promise.then((msg)=>console.log(msg))
+.catch((err)=>console.log(err));
+
+// settimeout
+
+let promise2 = new Promise((resolve, reject)=>{
+    console.log("hello");
+    setTimeout(()=>{console.log("time set")},2000)
+});
+promise.then((msg)=>console.log(msg))
+.catch((err)=>console.log(err));
+
+
+
+
+// json.js
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+  .then((response) => response.json())
+  .then((data) => { console.log("✅ API Data Received:", data);
+  })
+  .catch((error) => { console.error("❌ API Error:", error);
+ });
+
+ 
+
+
+
+
+
+
