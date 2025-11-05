1f70823ea80be08dcf0c69d9ed98923cdce4ddbc
SATISHSP-05 <satishsp0503@gmail.com>
2025-11-06 00:59:13 +0530

    promise data fetch

--- async_await.js
+++ async_await.js
@@ -27,14 +27,42 @@
 
 
 
-// use data portions
-fetch("https://jsonplaceholder.typicode.com/users/1")
-  .then((response) => response.json())
-  .then((data) => {
-    const { name, email } = data;          // destructuring
-    const { city } = data.address;         // nested destructuring
+// // use data portions
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const { name, email } = data;          
+//     const { city } = data.address;         
 
-    console.log(`User ${name} lives in ${city} and can be reached at ${email}.`);
-  })
-  .catch((error) => console.log("Error:", error));
+//     console.log(`User ${name} lives in ${city} and can be reached at ${email}.`);
+//   })
+//   .catch((error) => console.log("Error:", error));
 
+
+// fetch("https://jsonplaceholder.typicode.com/users/1")
+//   .then((response) => response.json()) 
+//   .then((data) => console.log("userId",data.username))
+//   .catch((error) => console.log("Error:", error));
+
+
+
+// //  async function
+// async function getData() {
+//   return "Hello Async!";
+// }
+// getData().then(result => console.log(result)); 
+
+
+
+async function prac() { 
+  try {
+  let result =await new Promise((resolve,reject)=>{
+    setTimeout((msg)=>console.log("trial"),1000)
+  });
+  console.log(result)
+}
+catch(err){
+  console.log("error",err)
+}
+}
+prac();
