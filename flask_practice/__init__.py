67388578a3dd766b8c9298865c90501ea67d5f28
SATISHSP-05 <satishsp0503@gmail.com>
2025-12-09 14:42:08 +0530

    flask_simple_project

---
+++ flask_practice/__init__.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html")
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
